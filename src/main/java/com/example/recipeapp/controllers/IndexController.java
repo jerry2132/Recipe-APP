e2f9a5cc5126897828c03910a438cb72b57dccf7
jerry2132 <sumitsinghrawat34@gmail.com>
2023-09-14 15:08:14 +0530

    commit applied

--- src/main/java/com/example/recipeapp/controllers/IndexController.java
+++ src/main/java/com/example/recipeapp/controllers/IndexController.java
@@ -5,12 +5,13 @@ import com.example.recipeapp.domain.UnitOfMeasure;
 import com.example.recipeapp.repositories.CategoryRepository;
 import com.example.recipeapp.repositories.UnitOfMeasureRepository;
 import com.example.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
-
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -23,6 +24,7 @@ public class IndexController {
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model)
     {
+        log.debug("getting index page here");
 
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
