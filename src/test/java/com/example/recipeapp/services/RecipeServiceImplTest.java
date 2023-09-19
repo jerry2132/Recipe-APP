bfd6f864c87df9fa7776ff10cb48260dea211848
jerry2132 <sumitsinghrawat34@gmail.com>
2023-09-19 16:54:02 +0530

    commit applied

--- src/test/java/com/example/recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/com/example/recipeapp/services/RecipeServiceImplTest.java
@@ -10,6 +10,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.*;
@@ -34,18 +35,35 @@ public class RecipeServiceImplTest {
     }
 
     @Test
-    public void getRecipes() {
+    public void getRecipeById() throws Exception{
+
+            Recipe recipe = new Recipe();
+            recipe.setId(1L);
+            Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+            when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+            Recipe recipeReturned = recipeService.findById(1L);
+
+            assertNotNull("Null recipe returned", recipeReturned);
+            verify(recipeRepository, times(1)).findById(anyLong());
+            verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
+    public void getRecipes() throws Exception{
 
         Recipe recipe = new Recipe();
-        HashSet recipesData = new HashSet();
-        recipesData.add(recipe);
+        HashSet receipesData = new HashSet();
+        receipesData.add(recipe);
 
-        when(recipeRepository.findAll()).thenReturn(recipesData);
+        when(recipeService.getRecipes()).thenReturn(receipesData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
 
-        assertEquals( 1,recipes.size());
+        assertEquals(recipes.size(), 1);
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
 
 
 
