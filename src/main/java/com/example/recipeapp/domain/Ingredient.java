8eebd05d67b3111d6341d3d9bbb04064570d8d39
jerry2132 <sumitsinghrawat34@gmail.com>
2023-09-19 19:19:05 +0530

    commit applied

--- src/main/java/com/example/recipeapp/domain/Ingredient.java
+++ src/main/java/com/example/recipeapp/domain/Ingredient.java
@@ -13,23 +13,26 @@ public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
     private BigDecimal amount;
-   @ManyToOne
+
+    @ManyToOne
     private UnitOfMeasure uom;
+
     @ManyToOne
     private Recipe recipe;
 
     public Ingredient() {
     }
+
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
     }
 
-
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom,Recipe recipe) {
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
--- src/main/java/com/example/recipeapp/domain/Recipe.java
+++ src/main/java/com/example/recipeapp/domain/Recipe.java
@@ -12,12 +12,14 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
     private Integer prepTime;
     private Integer cookTime;
     private Integer servings;
     private String source;
     private String url;
+
     @Lob
     private String directions;
 
@@ -27,19 +29,20 @@ public class Recipe {
 
     @Lob
     private Byte[] image;
+
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
     @ManyToMany
     @JoinTable(name = "recipe_category",
-            joinColumns =@JoinColumn(name = "recipe_id"),
+            joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-
     private Set<Category> categories = new HashSet<>();
 
     public void setNotes(Notes notes) {
-
         this.notes = notes;
         notes.setRecipe(this);
     }
